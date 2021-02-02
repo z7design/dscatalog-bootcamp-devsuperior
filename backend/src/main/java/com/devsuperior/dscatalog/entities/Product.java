2d73c9fb0118c3483fe96a31eaa2ef42097282ee
Jose Mario Ramos <z7design@z7design.com.br>
2021-02-01 21:00:02 -0300

    Add front -web

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id	
