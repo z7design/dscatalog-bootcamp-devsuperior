da9d8a74c2103a1823c28b7de0185fd7c59cf9db
José Mário Ramos <z7design@z7design.com.br>
2021-02-10 21:07:24 -0300

    Finish product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import ProducCard from './components/ProductCard';
+import './style.scss';
 
 const Catalog = () => (
 
-    <div>
-    <h1>Catalogo</h1>
-</div>
+    <div className="catalog-container">
+        <div className="catalog-title">
+        <h1>Catalogo de produtos</h1>
+        </div>
+        <div className="catalog-products">
+            <ProducCard />
+            <ProducCard />
+            <ProducCard />
+            <ProducCard />
+            <ProducCard />
+            <ProducCard />
+        </div>
+    </div>
 
 );
 
