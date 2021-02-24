bbc2b5d5a069273c967b8532b7f09aa9c7338773
Jose Mario <z7design@z7design.com.br>
2021-02-24 19:44:16 -0300

    Atualizations the files

---
+++ front-web/src/core/components/Pagination/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pagination = () => {
+
+    return (
+        <div className="pagination-container">
+
+        </div>
+    );
+}
+
+export default Pagination;
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactComponent as MainImage} from 'core/assets/images/main-image.svg'
-import ButtonIcon from 'core/components/NavBar/BootonIcon';
+import ButtonIcon from 'core/components/BootonIcon';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
